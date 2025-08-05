f064d42da11be0424bbb5063cf0884e0c237d86e
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-08-05 14:21:28 +0000

    Mise à jour : ajout de l'URL webhook Stripe et configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -46,7 +46,6 @@ function App() {
 <Route path="/forgot-password" element={<ForgotPassword />} />
 <Route path="/reset-password/:token" element={<ResetPassword />} />
 <Route path="/success" element={<Success />} />
-<Route path="/checkout" element={<Checkout />} />
 
 
 <Route path="/search" element={
